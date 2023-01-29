b212772807d866f0f6e13b72ad3fb8a8b0d63b78
Kacper Mierkiewicz <kmierkiewicz02@gmail.com>
2023-01-29 13:04:18 +0100

    update js function of change background

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+{
 let pln = document.querySelector('.pln');
 let eur = document.querySelector('.eur');
 let exchange__form = document.querySelector('.exchange__form');
@@ -11,11 +12,18 @@ exchange__form.addEventListener("input", () => {
     eur.value = result.toFixed(2);
     
 });
+ const toggleBackground = () => {
+    const body = document.querySelector(".body");
+    const nextColorName = document.querySelector(".js-nextColorName");
 
-let body = document.querySelector(".body");
-let button = document.querySelector(".changeBackground");
+    body.classList.toggle("background--dark")
+    nextColorName.innerText = body.classList.contains("background--dark") ? "Zmień kolor tła na jasny" : "Zmień kolor tła na ciemny";
+ };
 
-button.addEventListener("click", () => {
-    body.classList.toggle("darkBackground");
-    button.innerText = body.classList.contains('darkBackground') ? "Zmień kolor tła na jasny" : "Zmień kolor tła na ciemny";
-})
+ const init = () => {
+    const changeBackgroundButton = document.querySelector(".js-changeBackgroundButton");
+    changeBackgroundButton.addEventListener("click", toggleBackground);
+ }
+
+ init();
+}
